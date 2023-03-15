ba37a28b048519570bd8c1b8105ca5b0acae36c1
knoldus <krishna.tiwari@knoldus.com>
2023-03-15 16:49:31 +0530

    fisrt-commit-group-c

---
+++ src/main/scala/SupriseTestGroupC.scala
@@ -0,0 +1,15 @@
+//QUE: square a list of integers and display the count of even and odd values in a list in scala.
+object SupriseTestGroupC extends App {
+  def SquareAndCountEvenOdd(listOfInt: List[Int], sqrd: Any): Unit = {
+    var countEven = 0
+    var countOdd = 0
+    var num
+    listOfInt.map(num * num
+    => sqrd
+    )
+    if sqrd % 2 == 0
+    countEven += 1
+    else countOdd += 1
+  }
+
+}
